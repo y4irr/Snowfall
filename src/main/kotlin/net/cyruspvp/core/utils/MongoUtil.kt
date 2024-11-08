c150b859a9a82ce139a16563abc933aa6edf5608
Yair Soto <y4ir180919@gmail.com>
2024-11-08 09:19:05 -0300

    chore(util): added MongoUtil

---
+++ src/main/kotlin/net/cyruspvp/core/utils/MongoUtil.kt
@@ -0,0 +1,23 @@
+package net.cyruspvp.core.utils
+
+import com.mongodb.client.model.ReplaceOptions
+import com.mongodb.client.model.UpdateOptions
+import org.bson.json.JsonMode
+import org.bson.json.JsonWriterSettings
+
+/*
+ * This project can't be redistributed without
+ * authorization of the developer
+ *
+ * Project @ Snowfall
+ * @author Yair © 2024
+ * Date: 08 - nov
+ */
+
+object MongoUtil {
+
+    val UPDATE_OPTIONS: UpdateOptions = UpdateOptions().upsert(true)
+    val REPLACE_OPTIONS: ReplaceOptions = ReplaceOptions().upsert(true)
+    val RELAXED_WRITE_SETTING: JsonWriterSettings = JsonWriterSettings.builder().outputMode(JsonMode.RELAXED).build()
+
+}
