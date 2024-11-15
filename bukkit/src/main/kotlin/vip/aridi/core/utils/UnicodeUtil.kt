3826a390758534fe936cb327096ea7c46f17d871
Yair Soto <y4ir180919@gmail.com>
2024-11-15 19:58:15 -0300

    feat(util): added UnicodeUtil to enhance the code style

---
+++ bukkit/src/main/kotlin/vip/aridi/core/utils/UnicodeUtil.kt
@@ -0,0 +1,46 @@
+package vip.aridi.core.utils
+
+import org.bukkit.ChatColor
+
+/*
+ * This project can't be redistributed without
+ * authorization of the developer
+ *
+ * Project @ Snowfall
+ * @author Yair © 2024
+ * Date: 15 - nov
+ */
+
+object UnicodeUtil {
+
+    @JvmStatic val BLOCK = "${ChatColor.BOLD}⬛"
+    @JvmStatic val COINS = "${ChatColor.BOLD}⛁"
+    @JvmStatic val SMALL_BLOCK = "${ChatColor.BOLD}▌"
+    @JvmStatic val HEART = "${ChatColor.BOLD}❤"
+    @JvmStatic val CAUTION = "${ChatColor.BOLD}⚠"
+    @JvmStatic val EXP_SYMBOL = "${ChatColor.BOLD}✪"
+    @JvmStatic val ALT_TOKENS_SYMBOL = "${ChatColor.BOLD}۞"
+    @JvmStatic val SMALL_ARROW = "${ChatColor.BOLD}►"
+    @JvmStatic val CURVED_ARROW_RIGHT = "${ChatColor.BOLD}➥"
+    @JvmStatic val TRIANGLE_ARROW_LEFT = "${ChatColor.BOLD}◀"
+    @JvmStatic val TRIANGLE_ARROW_RIGHT = "${ChatColor.BOLD}▶"
+    @JvmStatic val ARROW_HEAD_RIGHT = "${ChatColor.BOLD}➤"
+    @JvmStatic val DOT_SYMBOL = "${ChatColor.BOLD}●"
+    @JvmStatic val BULLETIN_POINT = "${ChatColor.BOLD}•"
+    @JvmStatic val DOUBLE_ARROW_LEFT = "${ChatColor.BOLD}«"
+    @JvmStatic val DOUBLE_ARROW_RIGHT = "${ChatColor.BOLD}»"
+    @JvmStatic val VERTICAL_LINE = "${ChatColor.BOLD}┃"
+    @JvmStatic val THICK_VERTICAL_LINE = "${ChatColor.BOLD}❙"
+    @JvmStatic val CHECK_MARK = "${ChatColor.BOLD}✔"
+    @JvmStatic val CROSSED_MARK = "${ChatColor.BOLD}✖"
+    @JvmStatic val CROSSED_SWORDS_SYMBOL = "${ChatColor.BOLD}⚔"
+    @JvmStatic val REFRESH_SYMBOL = "${ChatColor.BOLD}⟲"
+    @JvmStatic val FLAG_SYMBOL = "${ChatColor.BOLD}⚑"
+    @JvmStatic val ANCHOR_SYMBOL = "${ChatColor.BOLD}⚓"
+    @JvmStatic val ARROW_UP = "${ChatColor.BOLD}⬆"
+    @JvmStatic val MONEY_SYMBOL = "${ChatColor.BOLD}＄"
+    @JvmStatic val ARROW_DOWN = "${ChatColor.BOLD}⬇"
+    @JvmStatic val PRESTIGE_SYMBOL = "${ChatColor.BOLD}⭑"
+    @JvmStatic val ARROW_LEFT = "${ChatColor.BOLD}⬅"
+    @JvmStatic val ARROW_RIGHT = "${ChatColor.BOLD}➡"
+}
