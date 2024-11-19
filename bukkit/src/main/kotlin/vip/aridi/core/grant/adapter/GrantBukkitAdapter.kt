7af5e7409d210c401414399defcb4b415f99714e
Yair Soto <y4ir180919@gmail.com>
2024-11-19 13:47:24 -0300

    feat(grant): added grant events

---
+++ bukkit/src/main/kotlin/vip/aridi/core/grant/adapter/GrantBukkitAdapter.kt
@@ -0,0 +1,63 @@
+package vip.aridi.core.grant.adapter
+
+import vip.aridi.core.module.impl.system.GrantModule
+import org.bukkit.ChatColor
+import org.bukkit.entity.Player
+import vip.aridi.core.Snowfall
+import vip.aridi.core.grant.Grant
+import vip.aridi.core.grant.event.GrantApplyEvent
+import vip.aridi.core.grant.event.GrantExpireEvent
+import vip.aridi.core.grant.event.GrantRemoveEvent
+import vip.aridi.core.module.ModuleManager
+import java.util.*
+
+/*
+ * This project can't be redistributed without
+ * authorization of the developer
+ *
+ * Project @ Snowfall
+ * @author Yair © 2024
+ * Date: 19 - nov
+ */
+
+class GrantBukkitAdapter : GrantModule.GrantAdapter {
+
+    private val core = Snowfall.get()
+    private val server = core.server
+
+    override fun onGrantApply(uuid: UUID, grant: Grant) {
+        getPlayer(uuid)?.let { player ->
+            ModuleManager.permissionModule.update(player, true)
+            runEvent { server.pluginManager.callEvent(GrantApplyEvent(player, grant)) }
+        }
+    }
+
+    override fun onGrantChange(uuid: UUID, grant: Grant) {
+
+    }
+
+    override fun onGrantExpire(uuid: UUID, grant: Grant) {
+        getPlayer(uuid)?.let { player ->
+            grant.getRank()?.takeIf { it.hidden }?.let { rank ->
+                player.sendMessage("${ChatColor.LIGHT_PURPLE}Your ${rank.displayName}${ChatColor.LIGHT_PURPLE} rank has expired.")
+            }
+            ModuleManager.permissionModule.update(player, true)
+            runEvent { server.pluginManager.callEvent(GrantExpireEvent(player, grant)) }
+        }
+    }
+
+    override fun onGrantRemove(uuid: UUID, grant: Grant) {
+        getPlayer(uuid)?.let { player ->
+            ModuleManager.permissionModule.update(player, true)
+            runEvent { server.pluginManager.callEvent(GrantRemoveEvent(player, grant)) }
+        }
+    }
+
+    private fun getPlayer(uuid: UUID): Player? {
+        return server.getPlayer(uuid)
+    }
+
+    private fun runEvent(action: () -> Unit) {
+        server.scheduler.runTask(core, action)
+    }
+}
---
+++ bukkit/src/main/kotlin/vip/aridi/core/grant/event/GrantApplyEvent.kt
@@ -0,0 +1,27 @@
+package vip.aridi.core.grant.event
+
+import org.bukkit.entity.Player
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
+import vip.aridi.core.grant.Grant
+
+/*
+ * This project can't be redistributed without
+ * authorization of the developer
+ *
+ * Project @ Snowfall
+ * @author Yair © 2024
+ * Date: 19 - nov
+ */
+
+class GrantApplyEvent(val player: Player, val grant: Grant) : Event() {
+
+    override fun getHandlers(): HandlerList {
+        return handlerList
+    }
+
+    companion object {
+        @JvmStatic val handlerList = HandlerList()
+    }
+
+}
