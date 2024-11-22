647e7c7c72b953aa4d7fffab54e737a81cb249bc
Yair Soto <y4ir180919@gmail.com>
2024-11-22 19:27:28 -0300

    feat(util): added Callback util

---
+++ shared/src/main/kotlin/vip/aridi/core/util/Callback.kt
@@ -0,0 +1,16 @@
+package vip.aridi.core.util
+
+/*
+ * This project can't be redistributed without
+ * authorization of the developer
+ *
+ * Project @ Snowfall
+ * @author Yair © 2024
+ * Date: 22 - nov
+ */
+
+interface Callback<T> {
+
+    fun callback(callback: T)
+
+}
