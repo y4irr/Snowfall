98744cb9cafff802dce5a10cb83e78fc16f913ea
Yair Soto <y4ir180919@gmail.com>
2024-11-23 11:17:01 -0300

    feat: grant menu

---
+++ bukkit/src/main/kotlin/vip/aridi/core/grant/menu/apply/GrantButton.kt
@@ -0,0 +1,90 @@
+package vip.aridi.core.grant.menu.apply
+
+import org.bukkit.ChatColor
+import org.bukkit.Material
+import org.bukkit.conversations.ConversationFactory
+import org.bukkit.conversations.NullConversationPrefix
+import org.bukkit.entity.Player
+import org.bukkit.event.inventory.ClickType
+import vip.aridi.core.Snowfall
+import vip.aridi.core.grant.prompt.GrantProcessPrompt
+import vip.aridi.core.module.ModuleManager
+import vip.aridi.core.profile.Profile
+import vip.aridi.core.rank.Rank
+import vip.aridi.core.utils.CC
+import vip.aridi.core.utils.menus.button.Button
+
+/*
+ * This project can't be redistributed without
+ * authorization of the developer
+ *
+ * Project @ Snowfall
+ * @author Yair © 2024
+ * Date: 22 - nov
+ */
+
+class GrantButton(private val rank: Rank, private val target: Profile) : Button() {
+
+    override fun getName(player: Player?): String {
+        return CC.translate(rank.displayName)
+    }
+
+    override fun getDescription(player: Player?): MutableList<String> {
+        val description = mutableListOf<String>()
+        description.add("")
+        if (!rank.canBeGrantable) {
+            description.add("&cThat rank is not grantable!")
+        } else {
+            description.add(styleAction(ChatColor.GREEN, "CLICK", "to give that rank to a player."))
+        }
+        return CC.translateMutable(description)
+    }
+
+    override fun getMaterial(player: Player?): Material {
+        return Material.WOOL
+    }
+
+    override fun getDamageValue(player: Player?): Byte {
+        return CC.getWoolData(rank.color)
+    }
+
+    override fun clicked(player: Player, slot: Int, clickType: ClickType) {
+        if (!isRankGrantable(player)) return
+
+        player.closeInventory()
+        initiateGrantProcess(player)
+    }
+
+    /**
+     * Checks if the rank can be granted, sending error messages if not.
+     */
+    private fun isRankGrantable(player: Player): Boolean {
+        if (rank == ModuleManager.rankModule.defaultRank) {
+            player.sendMessage(CC.translate("&cThat rank can't be granted as it is the default rank."))
+            return false
+        }
+
+        if (ModuleManager.grantModule.findAllByPlayer(target.id).any { it.getRank()?.name == rank.name && !it.isRemoved() }) {
+            player.sendMessage(CC.translate("&cGrant cannot be given as the player already possesses it."))
+            return false
+        }
+
+        return true
+    }
+
+    /**
+     * Initiates the grant process by starting a conversation with the player.
+     */
+    private fun initiateGrantProcess(player: Player) {
+        val conversationFactory = ConversationFactory(Snowfall.get())
+            .withModality(true)
+            .withPrefix(NullConversationPrefix())
+            .withFirstPrompt(GrantProcessPrompt(rank, target, player))
+            .withEscapeSequence("/no")
+            .withLocalEcho(false)
+            .withTimeout(25)
+            .thatExcludesNonPlayersWithMessage(CC.translate("&cOnly players can interact with this process!"))
+
+        conversationFactory.buildConversation(player).begin()
+    }
+}
---
+++ bukkit/src/main/kotlin/vip/aridi/core/grant/menu/apply/GrantMenu.kt
@@ -0,0 +1,88 @@
+package vip.aridi.core.grant.menu.apply
+
+import org.bukkit.entity.Player
+import vip.aridi.core.module.ModuleManager
+import vip.aridi.core.profile.Profile
+import vip.aridi.core.rank.Rank
+import vip.aridi.core.utils.CC
+import vip.aridi.core.utils.menus.PaginatedMenu
+import vip.aridi.core.utils.menus.button.Button
+import vip.aridi.core.utils.menus.button.impl.GlassButton
+
+/*
+ * This project can't be redistributed without
+ * authorization of the developer
+ *
+ * Project @ Snowfall
+ * @author Yair © 2024
+ * Date: 22 - nov
+ */
+
+class GrantMenu(
+    private val profile: Profile
+) : PaginatedMenu() {
+
+    init {
+        isUpdateAfterClick = true
+        isAutoUpdate = true
+    }
+
+    override fun getPrePaginatedTitle(player: Player?): String {
+        return CC.translate("&6&lChoose a Rank")
+    }
+
+    override fun getAllPagesButtons(player: Player?): MutableMap<Int, Button> {
+        val availableRanks = getAvailableRanks(player)
+        return availableRanks.mapIndexed { index, rank ->
+            index to GrantButton(rank, profile)
+        }.toMap().toMutableMap()
+    }
+
+    override fun getGlobalButtons(player: Player?): MutableMap<Int, Button> {
+        return buildGlassButtons()
+    }
+
+    override fun size(player: Player?): Int {
+        return 45
+    }
+
+    override fun getMaxItemsPerPage(player: Player?): Int {
+        return 27
+    }
+
+    /**
+     * Retrieves a list of ranks available to the player based on their permissions.
+     */
+    private fun getAvailableRanks(player: Player?): List<Rank> {
+        if (player == null) return emptyList()
+
+        val playerProfile = ModuleManager.profileModule.getProfile(player.uniqueId) ?: return emptyList()
+        val grantedRank = ModuleManager.grantModule.findGrantedRank(player.uniqueId)
+
+        return ModuleManager.rankModule.findAllRanks().filter { rank ->
+            rank.isEligibleForGrant(playerProfile, grantedRank.priority)
+        }.sortedByDescending { it.priority }
+    }
+
+    /**
+     * Builds the decorative glass buttons for the menu.
+     */
+    private fun buildGlassButtons(): MutableMap<Int, Button> {
+        val buttons = mutableMapOf<Int, Button>()
+        for (i in 0..8) {
+            buttons[getSlot(i, 0)] = GlassButton(7)
+            buttons[getSlot(i, 4)] = GlassButton(8)
+        }
+        return buttons
+    }
+}
+
+/**
+ * Extension function to determine if a rank is eligible for granting.
+ */
+private fun Rank.isEligibleForGrant(profile: Profile, grantedRankPriority: Int): Boolean {
+    return when {
+        profile.root -> grantedRankPriority >= this.priority
+        else -> !this.defaultRank && !this.hidden && grantedRankPriority >= this.priority
+    }
+}
