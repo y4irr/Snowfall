2e7251bf574219794a437fa9160d188ee628b915
Yair Soto <y4ir180919@gmail.com>
2024-11-28 22:18:22 -0300

    feat: Enchantment class command

---
+++ bukkit/src/main/kotlin/vip/aridi/core/command/essentials/EnchantmentCommand.kt
@@ -0,0 +1,31 @@
+package vip.aridi.core.command.essentials
+
+import com.jonahseguin.drink.annotation.Command
+import com.jonahseguin.drink.annotation.Require
+import com.jonahseguin.drink.annotation.Sender
+import org.bukkit.command.CommandSender
+
+/*
+ * This project can't be redistributed without
+ * authorization of the developer
+ *
+ * Project @ Snowfall
+ * @author Yair © 2024
+ * Date: 25 - nov
+ */
+
+class EnchantmentCommand {
+
+    @Command(
+        name = "",
+        desc = ""
+    )
+    @Require(
+        ""
+    )
+    fun enchantItem(
+        @Sender sender: CommandSender
+    ) {
+        
+    }
+}
